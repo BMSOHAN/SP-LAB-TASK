e636cdb84758f02889cd2d021553df29d048f21d
BMSOHAN <fazlaelahisohan@gmail.com>
2024-09-24 14:04:55 +0600

    Update add Task1-Odd-Even.cpp

--- add Task1-Odd-Even.cpp
+++ add Task1-Odd-Even.cpp
@@ -1,1 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, sum;
+
+
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+
+
+    sum = num1 + num2;
+
+
+    if (sum % 2 == 0) {
+        printf("The sum is %d and it is even.\n", sum);
+    } else {
+        printf("The sum is %d and it is odd.\n", sum);
+    }
+
+    return 0;
+}
+
 
