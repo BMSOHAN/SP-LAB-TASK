90fb756b9f13d808290ba025372d19edd7cc9972
BMSOHAN <fazlaelahisohan@gmail.com>
2024-10-01 22:34:43 +0600

    Task 2-Odd or Even.cpp

---
+++ Task 2-Odd or Even.cpp
@@ -0,0 +1,20 @@
+                    // Problem 02..
+                // Odd or Even....
+
+
+  #include<stdio.h>
+     int main(){
+
+    int a;
+     printf("Enter the number:\n");
+        scanf("%d",&a);
+
+          if (a % 2 == 0){
+            printf(" Even\n");
+          }else {
+             printf(" Odd\n");
+          }
+
+   return 0;
+
+     }
