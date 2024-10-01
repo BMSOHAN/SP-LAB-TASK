87318f3d185408b3c2cc46d4a9125173cc4cdd54
BMSOHAN <fazlaelahisohan@gmail.com>
2024-10-01 23:07:04 +0600

    Task13:Calculate Average.cpp

---
+++ Task13:Calculate Average.cpp
@@ -0,0 +1,14 @@
+                    //Problem 13
+                     //Calculate Average..
+
+        #include<stdio.h>
+           int main (){
+
+    float a,b,c,d,e,average;
+      printf("Enter any Five numbers:\n");
+        scanf("%f %f %f %f %f",&a,&b,&c,&d,&e);
+           average = (a+b+c+d+e)/5;
+              printf("Their Average Value is: %.3f",average);
+
+         return 0;
+            }
