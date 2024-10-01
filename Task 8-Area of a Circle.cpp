3ef9c417a93eb8881c8df42fba9c355509bfb0b1
BMSOHAN <fazlaelahisohan@gmail.com>
2024-10-01 22:52:00 +0600

    Task 8-Area of a Circle.cpp

---
+++ Task 8-Area of a Circle.cpp
@@ -0,0 +1,17 @@
+                    //Problem 08
+                     // Area of a Circle..
+
+
+       #include<stdio.h>
+          int main(){
+
+    int radius;
+    float area,Pi=3.14;
+        printf("Enter the radius of a circle =\n");
+          scanf(" %d",&radius);
+        area=Pi*radius*radius;
+      printf("area of a circle is = %.3f",area);
+
+
+  return 0;
+          }
