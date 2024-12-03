8e4115c3fa2be8e3f059532ff4e40bf73ccf83de
BMSOHAN <fazlaelahisohan@gmail.com>
2024-12-03 13:08:33 +0600

    Task11:Array Reversal.cpp

--- Task11:Array Reversal.cpp
+++ Task11:Array Reversal.cpp
@@ -18,3 +18,19 @@ int main()
       }
     return 0;
 }
+
+
+#include<stdio.h>
+int main() {
+int n;
+scanf("%d", &n);
+int arr[n];
+for(int i = 0; i < n; i++) {
+scanf("%d", &arr[i]);
+}
+printf("Reversed: ");
+for(int i = n - 1; i >= 0; i--) {
+printf("%d ", arr[i]);
+}
+return 0;
+}
